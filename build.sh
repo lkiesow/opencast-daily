dbc25a3fa8cef3d88ff9354c01cda7f45cdb058d
Lars Kiesow <lkiesow@uos.de>
2020-03-12 00:36:33 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+OUTDIR=~/output/
+
+cd
+pwd
+
+# Clone Opencast repository
+rm -rf opencast || :
+git clone https://github.com/opencast/opencast.git
+cd opencast
+
+branches="$(git branch -r | sed -n 's_^.*upstream/r/_r/_p' | sort -h | tail -n2) develop"
+
+for branch in $branches; do
+  cd modules
+  git clean -fdx .
+  cd ..
+  git checkout "${branch}"
+  mvn clean install \
+    --batch-mode \
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+    -DskipTests
+done
---
+++ readme.md
@@ -0,0 +1,3 @@
+Opencast Daily Builder
+======================
+
